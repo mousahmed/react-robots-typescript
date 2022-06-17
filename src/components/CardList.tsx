ef787a49a3d04a91d646355c9dfd80e1034deffb
mousahmed <mous.ahmed@outlook.com>
2022-06-17 10:46:15 +0200

    Added Type to all components and the App container

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,23 +1,22 @@
-import * as React from 'react';
-import Card from './Card';
+import * as React from "react";
+import Card from "./Card";
+import { IRobot } from "../containers/App";
 
-const CardList = ({ robots }) => {
-  return (
-    <div>
-      {
-        robots.map((user, i) => {
-          return (
-            <Card
-              key={i}
-              id={robots[i].id}
-              name={robots[i].name}
-              email={robots[i].email}
-              />
-          );
-        })
-      }
-    </div>
-  );
-}
+const CardList = ({ robots }: { robots: Array<IRobot> }) => {
+	return (
+		<div>
+			{robots.map((user, i) => {
+				return (
+					<Card
+						key={i}
+						id={robots[i].id}
+						name={robots[i].name}
+						email={robots[i].email}
+					/>
+				);
+			})}
+		</div>
+	);
+};
 
 export default CardList;
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,16 +1,20 @@
-import * as React from 'react';
+import * as React from "react";
 
-const SearchBox = ({ searchChange }) => {
-  return (
-    <div className='pa2'>
-      <input
-        className='pa3 ba b--green bg-lightest-blue'
-        type='search'
-        placeholder='search robots'
-        onChange={searchChange}
-      />
-    </div>
-  );
+interface ISearchBoxProps {
+	searchChange(event: React.SyntheticEvent<HTMLInputElement>): void;
+}
+
+const SearchBox = ({ searchChange }: ISearchBoxProps) => {
+	return (
+		<div className="pa2">
+			<input
+				className="pa3 ba b--green bg-lightest-blue"
+				type="search"
+				placeholder="search robots"
+				onChange={searchChange}
+			/>
+		</div>
+	);
 };
 
 export default SearchBox;
